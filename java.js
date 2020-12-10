7a091273d2a1f79d07cd779eedfd3aa150959166
Letspra <75735659+Letspra@users.noreply.github.com>
2020-12-11 01:20:50 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,21 @@
+$('.handle').on('click', function(){
+    $('nav ul').toggleClass('showing');
+  });
+  
+  $(document).ready(function(){
+        $(".roledetails1").click(function(){
+             $(".listeddetails1").toggle();
+        });
+  });
+  
+  $(document).ready(function(){
+        $(".roledetails2").click(function(){
+             $(".listeddetails2").toggle();
+        });
+  });
+  
+  $(document).ready(function(){
+        $(".roledetails3").click(function(){
+             $(".listeddetails3").toggle();
+        });
+  });
